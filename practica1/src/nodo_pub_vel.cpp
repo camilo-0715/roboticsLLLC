4ec4c3f2b715f039edf3988ff22b551d4ecf6b5f
Lucía Hernández Martín <l.hernandezm.2019@alumnos.urjc.es>
2021-02-17 22:57:44 +0100

    prueba_codigo
    Signed-off-by: Lucía Hernández Martín <l.hernandezm.2019@alumnos.urjc.es>

---
+++ practica1/src/nodo_pub_vel.cpp
@@ -0,0 +1,26 @@
+#include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "num_publisher");
+  ros::NodeHandle n;
+
+  ros::Publisher num_pub = n.advertise<geometry_msgs::Twist>("/mobile_base/command/velocity", 1);
+
+  ros::Rate loop_rate(10);
+
+  int count=0;
+
+  while (ros::ok())
+  {
+    geometry_msgs::Twist msg;
+
+    num_pub.publish(msg);
+
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+
+  return 0;
+ }

325a61b539dec153aecb71fcd4fa742a401ed14f
Chuismiguel <luismilopez35@gmail.com>
2021-02-18 21:44:54 +0100

    pequeños cambios y nodos no usados eliminados
    Signed-off-by: Chuismiguel <luismilopez35@gmail.com>

--- practica1/src/nodo_p1.cpp
+++ practica1/src/nodo_p1.cpp
@@ -7,8 +7,8 @@ void messageCallback(const geometry_msgs::Twist::ConstPtr& msg)
   printf("\ty: %f\n", msg->linear.y);
   printf("\tz: %f\n", msg->linear.z);
   printf("Angular\n\tx: %f\n", msg->angular.x);
-  printf("\ty: %f\n", msg->angular.x);
-  printf("\tz:  %f\n", msg->angular.x);
+  printf("\ty: %f\n", msg->angular.y);
+  printf("\tz:  %f\n", msg->angular.z);
   
 }
 

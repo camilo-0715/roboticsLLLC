18c84b41c4edde335f3b72f96ae63105974d3057
Lucía Hernández Martín <l.hernandezm.2019@alumnos.urjc.es>
2021-02-18 17:12:35 +0100

    cambio_callback
    Signed-off-by: Lucía Hernández Martín <l.hernandezm.2019@alumnos.urjc.es>

--- practica1/src/nodo_sub_bumper.cpp
+++ practica1/src/nodo_sub_bumper.cpp
@@ -6,9 +6,9 @@ void messageCallback(const geometry_msgs::Twist::ConstPtr& msg)
   printf("Linear\n\tx: %f\n", msg->linear.x);
   printf("\ty: %f\n", msg->linear.y);
   printf("\tz: %f\n", msg->linear.z);
-  printf("Angular\n\tx: %f\n", msg->linear.x);
-  printf("\ty: %f\n", msg->linear.x);
-  printf("\tz: %f\n", msg->linear.x);
+  printf("Angular\n\tx: %f\n", msg->angular.x);
+  printf("\ty: %f\n", msg->angular.x);
+  printf("\tz: %f\n", msg->angular.x);
   
 }
 

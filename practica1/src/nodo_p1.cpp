d9d2bb5d28d1af3771f799c2b5d8ce237f2ba091
Lucía Hernández Martín <l.hernandezm.2019@alumnos.urjc.es>
2021-02-19 01:24:05 +0100

    v(x) = 0 cuando choca
    Signed-off-by: Lucía Hernández Martín <l.hernandezm.2019@alumnos.urjc.es>

--- practica1/src/nodo_p1.cpp
+++ practica1/src/nodo_p1.cpp
@@ -1,5 +1,8 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
+#include "kobuki_msgs/BumperEvent.h"
+
+bool bump;
 
 void messageCallback(const geometry_msgs::Twist::ConstPtr& msg)
 {
@@ -8,9 +11,14 @@ void messageCallback(const geometry_msgs::Twist::ConstPtr& msg)
   printf("\tz: %f\n", msg->linear.z);
   printf("Angular\n\tx: %f\n", msg->angular.x);
   printf("\ty: %f\n", msg->angular.y);
-  printf("\tz:  %f\n", msg->angular.z);
+  printf("\tz: %f\n", msg->angular.z);
   
 }
+void bumperProcess(const kobuki_msgs::BumperEvent bumperMessage)
+{
+  if (bumperMessage.state == kobuki_msgs::BumperEvent::PRESSED) {bump = true;}
+  else {bump = false;} 
+}    
 
 int main(int argc, char **argv)
 {
@@ -18,16 +26,20 @@ int main(int argc, char **argv)
   ros::NodeHandle n;
 
   ros::Subscriber sub = n.subscribe("/mobile_base/commands/velocity", 1, messageCallback);
-  ros::Publisher num_pub = n.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 1);
+  ros::Subscriber sub2 = n.subscribe("/mobile_base/events/bumper", 1, bumperProcess);
 
-  ros::Rate loop_rate(1);
+  ros::Publisher num_pub1 = n.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 1);
+
+  ros::Rate loop_rate(40);
 
   while (ros::ok())
   {
-    geometry_msgs::Twist msg;
+    geometry_msgs::Twist msg1;
 
-    msg.linear.x = 0.5;
-    num_pub.publish(msg);
+    if (bump){msg1.linear.x = 0;}
+    else {msg1.linear.x = 1;}
+
+    num_pub1.publish(msg1);
 
     ros::spinOnce();
     loop_rate.sleep();

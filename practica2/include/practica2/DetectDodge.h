1c77fb43370dfcbe621b9bc4d54ce4bc90ee4535
juan camilo <jc.carmona.2019@alumnos.urjc.es>
2021-03-02 14:55:44 +0100

    corregi el estilo, tabulando bien y poniendo bien a las llaves

--- practica2/include/practica2/DetectDodge.h
+++ practica2/include/practica2/DetectDodge.h
@@ -9,41 +9,41 @@
 
 namespace practica2
 {
-class DetectDodge  
-{
-  public:
-    DetectDodge();
-    void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg);
-    void step();
+  class DetectDodge
+  {
+    public:
+      DetectDodge();
+      void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg);
+      void step();
 
-  private:
-    ros::NodeHandle n;
+    private:
+      ros::NodeHandle n;
 
-    enum movement {GOING_FORWARD = 0, GOING_BACK, TURNING}; // habría que hacer 2 giros distintos TURNING_L y TURNING_R, de momento lo dejo así para ir provando
+      enum movement {GOING_FORWARD = 0, GOING_BACK, TURNING}; // habría que hacer 2 giros distintos TURNING_L y TURNING_R, de momento lo dejo así para ir provando
 
-    /*
-    static const int GOING_FORWARD   = 0;
-    static const int GOING_BACK   = 1;
-    static const int TURNING     = 2;
-    */
+      /*
+      static const int GOING_FORWARD   = 0;
+      static const int GOING_BACK   = 1;
+      static const int TURNING     = 2;
+      */
 
-    const double TURNING_TIME = 1.0; //esto hay que mirarlo solo hepuesto una cifra para que no de error de compilacion
-    const double BACKING_TIME = 3.0;
+      const double TURNING_TIME = 1.0; //esto hay que mirarlo solo hepuesto una cifra para que no de error de compilacion
+      const double BACKING_TIME = 3.0;
 
-    const double OBSTACLE_DISTANCE = 0.5;
-    int state_ = 0;
+      const double OBSTACLE_DISTANCE = 0.5;
+      int state_ = 0;
 
-    bool detect_object_left_;
-    bool detect_object_center_;
-    bool detect_object_right_;
+      bool detect_object_left_;
+      bool detect_object_center_;
+      bool detect_object_right_;
 
 
-    ros::Time detect_ts_;
-    ros::Time turn_ts_;
+      ros::Time detect_ts_;
+      ros::Time turn_ts_;
 
-    ros::Subscriber sub_laser_;
-    ros::Publisher pub_vel_;
-};
+      ros::Subscriber sub_laser_;
+      ros::Publisher pub_vel_;
+  };
 }
 
 #endif  // PRACTICA2_DETECTEDODGE_H

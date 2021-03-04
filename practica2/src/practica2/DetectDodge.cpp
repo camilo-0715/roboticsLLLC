08cc1e142ba3d8c31c11ef0cf487dadafea146c3
lucia <ll.chen.2019@alumnos.urjc.es>
2021-03-04 03:39:41 +0100

    cambio de los nombres de los markers y sus tamaños

--- practica2/src/practica2/DetectDodge.cpp
+++ practica2/src/practica2/DetectDodge.cpp
@@ -111,61 +111,60 @@ namespace practica2
   void 
   DetectDodge::initMarkers()
   {
-    // marker: izuierda; marker2: centro; marker3: derecha
-    visualization_msgs::Marker marker;
-    marker.header.frame_id = "base_link";
-    marker.header.stamp = ros::Time();
-    marker.ns = "my_namespace";
-    marker.id = 0;
-    marker.type = visualization_msgs::Marker::SPHERE;
-    marker.action = visualization_msgs::Marker::ADD;
-    marker.pose.position.x = 0.25;
-    marker.pose.position.y = -0.43;
-    marker.pose.position.z = 0;
-    marker.pose.orientation.x = 0.0;
-    marker.pose.orientation.y = 0.0;
-    marker.pose.orientation.z = 0.0;
-    marker.pose.orientation.w = 1.0;
-    marker.scale.x = 0.25;
-    marker.scale.y = 0.25;
-    marker.scale.z = 0.25;
-    marker.color.a = 1.0; // Don't forget to set the alpha!
-    marker.color.r = 0.0;
-    marker.color.g = 1.0;
-    marker.color.b = 0.0;
+    visualization_msgs::Marker left_marker;
+    left_marker.header.frame_id = "base_link";
+    left_marker.header.stamp = ros::Time();
+    left_marker.ns = "my_namespace";
+    left_marker.id = 0;
+    left_marker.type = visualization_msgs::Marker::SPHERE;
+    left_marker.action = visualization_msgs::Marker::ADD;
+    left_marker.pose.position.x = 0.25;
+    left_marker.pose.position.y = 0.43;
+    left_marker.pose.position.z = 0;
+    left_marker.pose.orientation.x = 0.0;
+    left_marker.pose.orientation.y = 0.0;
+    left_marker.pose.orientation.z = 0.0;
+    left_marker.pose.orientation.w = 1.0;
+    left_marker.scale.x = 0.1;
+    left_marker.scale.y = 0.1;
+    left_marker.scale.z = 0.1;
+    left_marker.color.a = 1.0; // Don't forget to set the alpha!
+    left_marker.color.r = 0.0;
+    left_marker.color.g = 1.0;
+    left_marker.color.b = 0.0;
     //msg.lifetime = ros::Duration(1.0);
 
-    visualization_msgs::Marker marker2;
-    marker2 = marker;
-    marker2.id = 1;
-    marker2.pose.position.x = 0.5;
-    marker2.pose.position.y = 0;
+    visualization_msgs::Marker center_marker;
+    center_marker = left_marker;
+    center_marker.id = 1;
+    center_marker.pose.position.x = 0.5;
+    center_marker.pose.position.y = 0;
 
-    visualization_msgs::Marker marker3;
-    marker3 = marker; 
-    marker3.id = 2;  
-    marker3.pose.position.x = 0.25;
-    marker3.pose.position.y = 0.43;
+    visualization_msgs::Marker right_marker;
+    right_marker = left_marker; 
+    right_marker.id = 2;  
+    right_marker.pose.position.x = 0.25;
+    right_marker.pose.position.y = -0.43;
 
-    // Cambio de color
+    // Change color
     if(detect_object_left_){
-      marker.color.r = 1.0;
-      marker.color.g = 0.0;
+      left_marker.color.r = 1.0;
+      left_marker.color.g = 0.0;
     }
     if(detect_object_center_){
-      marker2.color.r = 1.0;
-      marker2.color.g = 0.0;
+      center_marker.color.r = 1.0;
+      center_marker.color.g = 0.0;
     }
     if(detect_object_right_){
-      marker3.color.r = 1.0;
-      marker3.color.g = 0.0;
+      right_marker.color.r = 1.0;
+      right_marker.color.g = 0.0;
     }
 
     visualization_msgs::MarkerArray marker_array;
     marker_array.markers.resize(3);
-    marker_array.markers[0] = marker;
-    marker_array.markers[1] = marker2;
-    marker_array.markers[2] = marker3;
+    marker_array.markers[0] = left_marker;
+    marker_array.markers[1] = center_marker;
+    marker_array.markers[2] = right_marker;
 
     marker_pub.publish(marker_array);
     

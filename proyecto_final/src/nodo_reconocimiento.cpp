d6ec64c9028aad6cd02005efb1d5982a93a23701
Chuismiguel <luismilopez35@gmail.com>
2021-05-02 21:16:01 +0200

    structuring and bugfixing
    Signed-off-by: Chuismiguel <luismilopez35@gmail.com>

---
+++ proyecto_final/src/nodo_reconocimiento.cpp
@@ -0,0 +1,12 @@
+#include "proyecto_final/coord_reader.hpp"
+#include "proyecto_final/movement.hpp"
+
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "bedroom");
+  // code here
+  ros::Rate loop_rate(7);
+
+  return 0;
+}

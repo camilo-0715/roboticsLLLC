0d1914d5c4c35ba447a1c0efed88c99e1e88dd84
juan camilo <jc.carmona.2019@alumnos.urjc.es>
2021-05-13 16:52:39 +0200

    implemented front.cpp approach.cpp and their hpps

--- proyecto_final/src/proyecto_final/inFront.cpp
+++ proyecto_final/src/proyecto_final/inFront.cpp
@@ -4,9 +4,44 @@ namespace proyecto_final
 {
 
 inFront::inFront(const std::string& name, const BT::NodeConfiguration & config) // implemented this to check if an object is in front of another.
-: BT::ActionNodeBase(name, config) // it should have a method called inFront that returns a boolean and just checks the alignment of the TF with it.
+: BT::ActionNodeBase(name, config), buffer_(),listener_(buffer_) // it should have a method called inFront that returns a boolean and just checks the alignment of the TF with it.
 {
+  pub_vel_=  nh_.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 1);
 }
+
+int 
+inFront::turnTo_TF()
+{
+  geometry_msgs::Twist cmd;
+
+  geometry_msgs::TransformStamped bf2object_msg;
+  try {
+    bf2object_msg = buffer_.lookupTransform("base_footprint",getInput<std::string>("object").value(), ros::Time(0));
+  } catch (std::exception & e) {
+    return -1;
+  }
+
+  double angle = atan2(bf2object_msg.transform.translation.y,bf2object_msg.transform.translation.x);
+  
+  if (angle > -ANGLE_INTERVAL && angle < ANGLE_INTERVAL) {
+    cmd.linear.x = 0;
+    cmd.angular.z = 0;
+    pub_vel_.publish(cmd);
+    return 0;
+  }
+  else {
+    cmd.linear.x = 0;
+    if (angle < 0) {
+      cmd.angular.z = -TURN_SPEED;
+    } else {
+      cmd.angular.z = TURN_SPEED;
+    }
+    pub_vel_.publish(cmd);
+    return 1;
+  }
+}
+
+
 
 /*
   Bool
@@ -25,9 +60,15 @@ inFront::tick()
 {
 
   ROS_INFO("inFront tick");
-  // if .... in Front... return BT::NodeStatus::SUCESS;
-  // else FAILURE
-  return BT::NodeStatus::FAILURE;
+  if( turnTo_TF() == 0) 
+  {
+    return BT::NodeStatus::SUCCESS;
+  }
+  else
+  {
+    return BT::NodeStatus::RUNNING;
+  }
+  
 }
 
 }  // namespace proyecto_final

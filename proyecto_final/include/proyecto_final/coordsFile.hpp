96d79a0aadb204393b3bc11bb4c99c2143947e9a
Chuismiguel <luismilopez35@gmail.com>
2021-05-18 14:00:07 +0200

    Changed Map.yaml and added old points
    Signed-off-by: Chuismiguel <luismilopez35@gmail.com>

--- proyecto_final/include/proyecto_final/coordsFile.hpp
+++ proyecto_final/include/proyecto_final/coordsFile.hpp
@@ -18,7 +18,20 @@ namespace coordsData
     const double point4_x = -6.813386599943721;
     const double point4_y = 0.9652352103068782;
 
+    const double point_A_x = 2.9544038;
+    const double point_A_y = 0.9137344;
+
+    const double point_B_x = 3.0112829;
+    const double point_B_y = -3.058941;
+
+    const double point_C_x = -0.626243;
+    const double point_C_y = -3.077353;
+
+    const double point_D_x = -0.191769;
+    const double point_D_y = 1.0076322;
+
     const double t_orientation = 1.0;
+
 
 }
 

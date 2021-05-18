ec63dea4219f5f9f655b9f99d84173e5da6f2396
lucia <ll.chen.2019@alumnos.urjc.es>
2021-05-18 18:05:04 +0200

    points changed

--- proyecto_final/include/proyecto_final/coordsFile.hpp
+++ proyecto_final/include/proyecto_final/coordsFile.hpp
@@ -6,7 +6,7 @@ namespace coordsData
     const double origin_x = 0.0; // copied from last proyect
     const double origin_y = 0.0;
 
-    const double point1_x = 5.811502762329705;
+    /*const double point1_x = 5.811502762329705;
     const double point1_y = 0.012321041930387383;
 
     const double point2_x = 5.922811973070979;
@@ -16,19 +16,19 @@ namespace coordsData
     const double point3_y = -2.7877783015825846;
 
     const double point4_x = -6.813386599943721;
-    const double point4_y = 0.9652352103068782;
+    const double point4_y = 0.9652352103068782;*/
 
-    const double point_A_x = 2.9544038;
-    const double point_A_y = 0.9137344;
+    const double point1_x = 2.9544038;
+    const double point1_y = 0.9137344;
 
-    const double point_B_x = 3.0112829;
-    const double point_B_y = -3.058941;
+    const double point2_x = 3.0112829;
+    const double point2_y = -3.058941;
 
-    const double point_C_x = -0.626243;
-    const double point_C_y = -3.077353;
+    const double point3_x = -0.626243;
+    const double point3_y = -3.077353;
 
-    const double point_D_x = -0.191769;
-    const double point_D_y = 1.0076322;
+    const double point4_x = -0.191769;
+    const double point4_y = 1.0076322;
 
     const double t_orientation = 1.0;
 
